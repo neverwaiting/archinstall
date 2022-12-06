ad5c1e13a549ef11dfe0136ac3ac56007c647f98
wintersun <nerverstop@163.com>
2022-12-06 21:10:18 +0800

    update

--- chroot.sh
+++ chroot.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+install() {
+  pacman --noconfirm --needed -S $@
+}
+
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 hwclock --systohc
 
@@ -26,7 +30,7 @@ chsh -s /bin/zsh "$name" >/dev/null 2>&1
 # set root password same with user's password
 echo -e "$password\n$password" | passwd
 
-pacman --noconfirm --needed -S networkmanager
+install networkmanager
 systemctl enable NetworkManager
 systemctl start NetworkManager
 
@@ -36,13 +40,13 @@ if [ -n "$(lscpu | awk '/Model name/' | grep AMD)" ]; then
 else
   UCODE="intel-ucode"
 fi
-pacman --noconfirm --needed -S grub efibootmgr $UCODE os-prober
+install grub efibootmgr $UCODE os-prober
 
 grub-install --target="$(uname -m)-efi" --efi-directory=/boot --removable
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # update mirror source
-pacman --noconfirm --needed -S reflector
+install reflector
 reflector --country China --latest 5 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist > /dev/null 2>&1
 cat << EOF >> /etc/pacman.conf
 [archlinuxcn]
@@ -60,28 +64,29 @@ if [ -z $driver ]; then driver="1"; fi
 case $driver in
   "1")
     # for intel
-    pacman --noconfirm --needed -S mesa vulkan-intel xf86-video-intel;;
+    install mesa vulkan-intel xf86-video-intel;;
   "2")
     # for amd
-    pacman --noconfirm --needed -S mesa vulkan-radeon xf86-video-amdgpu;;
+    install mesa vulkan-radeon xf86-video-amdgpu;;
   "3")
     # for nvidia
-    pacman --noconfirm --needed -S nvidia nvidia-settings nvidia-utils;;
+    install nvidia nvidia-settings nvidia-utils;;
 esac
 
-pacman --noconfirm --needed -S xorg xorg-server xorg-xinit
-pacman --noconfirm --needed -S openssh && systemctl enable sshd
-pacman --noconfirm --needed -S openvpn zsh git wget zip unzip ripgrep fd fzf cmake ccls htop benchmark man-pages
-pacman --noconfirm --needed -S go lua nodejs npm python python-pip
-pacman --noconfirm --needed -S lf feh picom libnotify dunst
-pacman --noconfirm --needed -S bat mediainfo ffmpegthumbnailer imagemagick\
- calcurse exiv2 sxiv xclip gimp zathura zathura-pdf-mupdf obs-studio ncmpcpp\
- mpd mpv noto-fonts noto-fonts-emoji pipewire pamixer pulseaudio pulsemixer\
- python-pywal ueberzug bmon
-pacman --noconfirm --needed -S net-tools firefox firefox-i18n-zh-cn
-pacman --noconfirm --needed -S alsa-utils
-pacman --noconfirm --needed -S figlet neofetch
-pacman --noconfirm --needed -S v2ray qv2ray
+install xorg xorg-server xorg-xinit
+install openssh && systemctl enable sshd
+install cronie && systemctl enable cronie
+install openvpn starship zsh git wget zip unzip ripgrep fd fzf cmake ccls htop benchmark man-pages
+install go lua nodejs npm python python-pip
+install lf feh picom libnotify dunst
+install bat mediainfo ffmpeg ffmpegthumbnailer imagemagick\
+ calcurse exiv2 sxiv xclip gimp zathura zathura-pdf-mupdf obs-studio \
+ mpv noto-fonts noto-fonts-emoji pipewire pamixer pulseaudio pulsemixer\
+ python-pywal ueberzug bmon yt-dlp lynx
+install net-tools brave-bin
+install alsa-utils
+install figlet neofetch
+install v2ray qv2ray
 
 USER_HOME="/home/$name"
 USER_LOCAL_HOME="$USER_HOME/.local"
@@ -89,8 +94,8 @@ USER_CONFIG_HOME="$USER_HOME/.config"
 MIRROR_GITHUB_URL="https://github.91chi.fun/https://github.com"
 
 # install input for chinese
-pacman --noconfirm --needed -S fcitx5-im fcitx5-chinese-addons fcitx5-lua fcitx5-pinyin-zhwiki
-pacman --noconfirm --needed -S adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts
+install fcitx5-im fcitx5-chinese-addons fcitx5-lua fcitx5-pinyin-zhwiki
+install adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts
 USER_FCITX_THEME_DIR="$USER_LOCAL_HOME/share/fcitx5/themes" 
 [ -d "$USER_FCITX_THEME_DIR" ] || sudo -u "$name" mkdir -p "$USER_FCITX_THEME_DIR"
 sudo -u "$name" git -C "$USER_FCITX_THEME_DIR" clone "$MIRROR_GITHUB_URL/sxqsfun/fcitx5-sogou-themes.git"
@@ -121,6 +126,9 @@ pushd "$USER_HOME/tools/yesplaymusic" && \
 sudo -u "$name" GOPROXY="https://goproxy.cn" makepkg --noconfirm -si && \
 popd && ln -sf /opt/YesPlayMusic/yesplaymusic /bin/yesplaymusic || echo -e "########## install yesplaymusic error! ##########\n"
 
+# install zsh syntax highlight plugin
+sudo -u "$name" yay -S --noconfirm zsh-fast-syntax-highlighting
+
 sudo -u "$name" yay -S --noconfirm lolcat
 
 # set dotfiles
@@ -128,21 +136,6 @@ sudo -u "$name" git clone "$MIRROR_GITHUB_URL/neverwaiting/dotfiles.git" "$USER_
 sudo -u "$name" cp -r "$USER_HOME/dotfiles/.config" "$USER_HOME/" && \
 sudo -u "$name" cp -r "$USER_HOME/dotfiles/.local" "$USER_HOME/" && \
 sudo -u "$name" cp "$USER_HOME/dotfiles/.zprofile" "$USER_HOME/" && \
-sudo -u "$name" cp "$USER_CONFIG_HOME/x11/xmodmap" "$USER_HOME/.Xmodmap" && \
 sudo -u "$name" cp "$USER_CONFIG_HOME/npm/npmrc" "$USER_HOME/.npmrc" || echo -e "########## set dotfiles error! ##########\n"
-
-# add font CascadiaCode
-USER_FONT_DIR="$USER_LOCAL_HOME/share/fonts"
-sudo -u "$name" mkdir -p $USER_FONT_DIR && \
-sudo -u "$name" wget "$MIRROR_GITHUB_URL/ryanoasis/nerd-fonts/releases/download/v2.2.2/CascadiaCode.zip" -O "$USER_FONT_DIR/CascadiaCode.zip" && \
-sudo -u "$name" unzip "$USER_FONT_DIR/CascadiaCode.zip" -d "$USER_FONT_DIR" || echo -e "########## add nerd font error! ##########\n"
-
-MIRROR_RAW_GITHUB_URL="https://github.91chi.fun/https://raw.github.com"
-# install oh-my-zsh and powerlevel10k theme
-sudo -u "$name" curl -fsL "$MIRROR_RAW_GITHUB_URL/ohmyzsh/ohmyzsh/master/tools/install.sh" | \
-  sed 's/https:\/\/github.com/https:\/\/github.91chi.fun\/https:\/\/github.com/g' > "$USER_HOME/omzinstall.sh"
-sudo -u "$name" ZSH="$USER_CONFIG_HOME/zsh/oh-my-zsh" sh "$USER_HOME/omzinstall.sh" --unattended && \
-sudo -u "$name" git clone --depth=1 "$MIRROR_GITHUB_URL/romkatv/powerlevel10k.git" "$USER_CONFIG_HOME/zsh/oh-my-zsh/custom/themes/powerlevel10k" && \
-rm -rf $USER_HOME/{.zshrc,omzinstall.sh} || echo -e "########## install oh-my-zsh or powerlevel10k error! ##########\n"
 
 rm -rf $USER_HOME/{.bash_logout,.bash_profile,.bashrc,tools,dotfiles}
